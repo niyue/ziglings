9a4d6844fab7905c7fc1bec40a2e1849ff900f61
Yue Ni <niyue.com@gmail.com>
2023-07-22 22:50:19 +0800

    getting started with devbox and ziglings

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -12,9 +12,12 @@ const NumError = error{IllegalNumber};
 pub fn main() void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
-
-    try stdout.print("my_num={}\n", .{my_num});
+    const my_num: NumError!u32 = getNumber();
+    if (my_num) |value| {
+        stdout.print("my_num={}\n", .{value}) catch return;
+    } else |err| switch (err) {
+        NumError.IllegalNumber => return
+    }
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
